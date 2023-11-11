f15ddba50689cb82e77f3fd04ade9ab42376c378
Ryan P <rdpinkstaff@gmail.com>
2023-11-11 09:13:10 -0700

    Mobile css mostly done

---
+++ scripts.js
@@ -0,0 +1,6 @@
+const imagesArray = ["./images/image-product-1",
+                     "./images/image-product-2",
+                     "./images/image-product-3",
+                     "./images/image-product-4"
+                    ];
+
